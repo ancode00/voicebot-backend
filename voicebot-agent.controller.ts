b2e8d60cf55dbe85ee85c9ab65418628b245bde5
Anchit Saluja <67328749+hairaat@users.noreply.github.com>
2025-04-22 19:43:37 +0530

    feat: added full CRUD support for voicebot-agent

---
+++ voicebot-agent.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, Post, Get, Put, Delete, Body, Param } from '@nestjs/common';
+import { VoicebotAgentService } from './voicebot-agent.service';
+
+@Controller('voicebot-agent')
+export class VoicebotAgentController {
+  constructor(private readonly agentService: VoicebotAgentService) {}
+
+  @Post()
+  create(@Body() body: any) {
+    return this.agentService.create(body);
+  }
+
+  @Get()
+  getAll() {
+    return this.agentService.getAll();
+  }
+
+  @Get(':id')
+  getById(@Param('id') id: string) {
+    return this.agentService.getById(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateDto: any) {
+    return this.agentService.update(id, updateDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.agentService.delete(id);
+  }
+}
+
