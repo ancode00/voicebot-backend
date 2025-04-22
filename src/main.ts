c0a4fef852883ffefb8f9cbea8ef125bb7723a21
Anchit Saluja <67328749+hairaat@users.noreply.github.com>
2025-04-22 18:38:14 +0530

    debug: log MONGO_URI on startup

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(' MONGO_URI:', process.env.MONGO_URI); // will show what Mongo URI is being used inside Render logs
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
